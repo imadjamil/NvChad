2a33d59cb5966ca37d56a5994add0a75b83fa86b
imad <imad.jamil@live.com>
2021-11-08 14:32:25 +0100

    autopairs: don't apply pair sif next char is alphanumeric

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -11,6 +11,8 @@ M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
 -- To use this file, copy the structure of `core/default_config.lua`,
 -- examples of setting relative number & changing theme:
 
+vim.g.pydocstring_doq_path = "/home/imad/.virtualenvs/itron_pts/bin/doq"
+
 M.options = {
    relativenumber = true,
    tabstop = 4, -- Number of spaces that a <Tab> in the file counts for
@@ -28,21 +30,24 @@ M.ui = {
 
 -- NvChad included plugin options & overrides
 M.plugins = {
-   status = {
-      clorizer = true,
-   },
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.plugins.lspconfig",
-      },
-   },
-   -- To change the Packer `config` of a plugin that comes with NvChad,
-   -- add a table entry below matching the plugin github name
-   --              '-' -> '_', remove any '.lua', '.nvim' extensions
-   -- this string will be called in a `require`
-   --              use "(custom.configs).my_func()" to call a function
-   --              use "custom.blankline" to call a file
-   default_plugin_config_replace = {},
+  status = {
+    clorizer = true,
+  },
+  options = {
+    lspconfig = {
+      setup_lspconf = "custom.plugins.lspconfig",
+    },
+    autopairs = {
+      ignored_next_char = "[%w%.]" -- will ignore alphanumeric and `.` symbol
+    },
+  },
+  -- To change the Packer `config` of a plugin that comes with NvChad,
+  -- add a table entry below matching the plugin github name
+  --              '-' -> '_', remove any '.lua', '.nvim' extensions
+  -- this string will be called in a `require`
+  --              use "(custom.configs).my_func()" to call a function
+  --              use "custom.blankline" to call a file
+  default_plugin_config_replace = {},
 }
 
 return M
