0e739776283106d819a7d8f483535f31ffc2e992
Imad <imad.jamil@itron.com>
2022-04-13 13:40:56 +0200

    Fixing

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -14,40 +14,43 @@ M.options, M.ui, M.mappings, M.plugins = {}, {}, {}, {}
 vim.g.pydocstring_doq_path = "/home/imad/.virtualenvs/itron_pts/bin/doq"
 
 M.options = {
-   relativenumber = true,
-   tabstop = 4, -- Number of spaces that a <Tab> in the file counts for
-   -- NvChad options
-   nvChad = {
-      -- used for updater
-      update_url = "https://github.com/imadjamil/NvChad",
-      update_branch = "main",
-   },
+  relativenumber = true,
+  tabstop = 4, -- Number of spaces that a <Tab> in the file counts for
+  -- NvChad options
+  nvChad = {
+    -- used for updater
+    update_url = "https://github.com/imadjamil/NvChad",
+    update_branch = "main",
+  },
 }
 
 M.ui = {
-   theme = "gruvchad",
+  theme = "gruvchad",
 }
+
+local userPlugins = require "custom.plugins" -- path to table
 
 -- NvChad included plugin options & overrides
 M.plugins = {
-   status = {
-      clorizer = true,
-   },
-   options = {
-      lspconfig = {
-         setup_lspconf = "custom.plugins.lspconfig",
-      },
-      autopairs = {
-         ignored_next_char = "[%w%.$]", -- will ignore alphanumeric and `.` symbol
-      },
-   },
-   -- To change the Packer `config` of a plugin that comes with NvChad,
-   -- add a table entry below matching the plugin github name
-   --              '-' -> '_', remove any '.lua', '.nvim' extensions
-   -- this string will be called in a `require`
-   --              use "(custom.configs).my_func()" to call a function
-   --              use "custom.blankline" to call a file
-   default_plugin_config_replace = {},
+  install = userPlugins,
+  status = {
+    clorizer = true,
+  },
+  options = {
+    lspconfig = {
+      setup_lspconf = "custom.plugins.lspconfig",
+    },
+    autopairs = {
+      ignored_next_char = "[%w%.$]", -- will ignore alphanumeric and `.` symbol
+    },
+  },
+  -- To change the Packer `config` of a plugin that comes with NvChad,
+  -- add a table entry below matching the plugin github name
+  --              '-' -> '_', remove any '.lua', '.nvim' extensions
+  -- this string will be called in a `require`
+  --              use "(custom.configs).my_func()" to call a function
+  --              use "custom.blankline" to call a file
+  default_plugin_config_replace = {},
 }
 
 return M
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,74 +1,72 @@
-local customPlugins = require("core.customPlugins")
+return {
+	{
+		"deoplete-plugins/deoplete-clang",
+		event = "InsertEnter",
+	},
 
-customPlugins.add(function(use)
-   use {
-      "deoplete-plugins/deoplete-clang",
-      event = "InsertEnter",
-   }
+	-- formatting and diagnostics
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.conf.null-ls").setup()
+		end,
+	},
 
-   -- formatting and diagnostics
-   use {
-      "jose-elias-alvarez/null-ls.nvim",
-      after = "nvim-lspconfig",
-      config = function()
-         require("custom.plugins.conf.null-ls").setup()
-      end,
-   }
+	{
+		"folke/trouble.nvim",
+		requires = "kyazdani42/nvim-web-devicons",
+		config = function()
+			require("trouble").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+			}
+		end,
+	},
 
-   use {
-      "folke/trouble.nvim",
-      requires = "kyazdani42/nvim-web-devicons",
-      config = function()
-         require("trouble").setup {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-         }
-      end,
-   }
+	-- refactoring and code manipulation
+	{
+		"tpope/vim-commentary",
+	},
 
-   -- refactoring and code manipulation
-   use {
-      "tpope/vim-commentary",
-   }
+	{
+		"heavenshell/vim-pydocstring",
+		run = "make install",
+	},
 
-   use {
-      "heavenshell/vim-pydocstring",
-      run = "make install",
-   }
+	-- note taking
+	-- use {
+	--    "oberblastmeister/neuron.nvim",
+	--    requires = {
+	--       "nvim-lua/popup.nvim",
+	--       "nvim-lua/plenary.nvim",
+	--       "nvim-telescope/telescope.nvim",
+	--    },
+	--    config = function()
+	--       require("custom.plugins.conf.neuron").setup()
+	--    end,
+	-- }
 
-   -- note taking
-   -- use {
-   --    "oberblastmeister/neuron.nvim",
-   --    requires = {
-   --       "nvim-lua/popup.nvim",
-   --       "nvim-lua/plenary.nvim",
-   --       "nvim-telescope/telescope.nvim",
-   --    },
-   --    config = function()
-   --       require("custom.plugins.conf.neuron").setup()
-   --    end,
-   -- }
+	-- git
+	{
+		"andrewradev/diffurcate.vim",
+	},
 
-   -- git
-   use {
-      "andrewradev/diffurcate.vim",
-   }
+	-- syntax highlighting
+	{
+		"numirias/semshi", -- python
+	},
 
-   -- syntax highlighting
-   use {
-      "numirias/semshi", -- python
-   }
-
-   -- navigation
-   use {
-      "ThePrimeagen/harpoon",
-      requires = {
-         "nvim-lua/plenary.nvim",
-         "nvim-lua/popup.nvim",
-      },
-      config = function()
-         require("custom.plugins.conf.harpoon").setup()
-      end,
-   }
-end)
+	-- navigation
+	{
+		"ThePrimeagen/harpoon",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"nvim-lua/popup.nvim",
+		},
+		config = function()
+			require("custom.plugins.conf.harpoon").setup()
+		end,
+	}
+}
